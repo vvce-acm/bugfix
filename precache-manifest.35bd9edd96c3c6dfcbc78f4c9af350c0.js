6f80bed1a29abe67d52b90bcf3b5b330b73abc36
vvce-acm <vvce.acm@gmail.com>
2020-07-01 00:03:24 +0530

    Updates

---
+++ precache-manifest.35bd9edd96c3c6dfcbc78f4c9af350c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754560e26b2961a37f2e25bdc528f8f0",
+    "url": "/bugfix/index.html"
+  },
+  {
+    "revision": "66777a056157f83bf267",
+    "url": "/bugfix/static/css/main.d6223b97.chunk.css"
+  },
+  {
+    "revision": "3b0f9711454fe739c7ae",
+    "url": "/bugfix/static/js/2.5c01c29f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bugfix/static/js/2.5c01c29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66777a056157f83bf267",
+    "url": "/bugfix/static/js/main.eff5def2.chunk.js"
+  },
+  {
+    "revision": "c429643407df74575efc",
+    "url": "/bugfix/static/js/runtime-main.2dcd8eb4.js"
+  },
+  {
+    "revision": "2200042556f3731367408c228f2f877d",
+    "url": "/bugfix/static/media/acmLogo.22000425.svg"
+  },
+  {
+    "revision": "0bf171a2f3ba91aff47084651b9d5651",
+    "url": "/bugfix/static/media/bug.0bf171a2.svg"
+  },
+  {
+    "revision": "f6b04bf767985c7492aea76d8e8fb6f5",
+    "url": "/bugfix/static/media/vvceLogo.f6b04bf7.svg"
+  }
+]);
