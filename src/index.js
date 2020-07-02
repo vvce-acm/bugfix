826580c1a031bfa6d081e5b1cb461724fd2aca43
Suraj-Gov <surajgovind332000@gmail.com>
2020-07-02 23:11:28 +0530

    added animations v1.2

--- src/index.js
+++ src/index.js
@@ -11,35 +11,36 @@ class Main extends Component {
   }
 
   componentDidMount() {
-    AOS.init({});
+    AOS.init({
+      delay: 200,
+    });
   }
 
   render() {
     return (
       <div className="container">
-        <div className="bug">
+        <div className="bug slide-in-elliptic-left-fwd">
           <img
             src={`${process.env.PUBLIC_URL}/images/better-bug.gif`}
             alt="bug"
           ></img>
           <h1>BugFix</h1>
         </div>
-        <div className="details">
+        <div className="details slide-in-bck-right">
           <div className="desc">
-            <p>BugFix is the first of it's kind contest.</p>
-            <p>
+            <p data-aos="fade-right">
+              BugFix is the first of it's kind contest.
+            </p>
+            <p data-aos="fade-left">
               You don't need to write code from scratch, it's already hosted for
               you.
             </p>
-            <p>
+            <p data-aos="fade-up">
               Just find the bugs in the code, squash them with your bug-free
               code, and make the program bug free!
             </p>
           </div>
-          <a href="https://forms.gle/nGuxmLWthdBdrhn7A">
-            <button>Register Now</button>
-          </a>
-          <div className="event-details">
+          <div data-aos="zoom-in-up" className="event-details">
             <div>
               <label htmlFor="date">EVENT DATE</label>
               <p id="date" className="detail">
@@ -53,21 +54,24 @@ class Main extends Component {
               </p>
             </div>
           </div>
-          <div className="logos">
-            <p>Powered By</p>
-            <a href="https://www.vvce.ac.in">
-              <img
-                src={`${process.env.PUBLIC_URL}/images/vvceLogo.png`}
-                alt="vvce"
-              ></img>
-            </a>
-            <a href="https://www.vvce.ac.org">
-              <img
-                src={`${process.env.PUBLIC_URL}/images/acmLogo.png`}
-                alt="acm"
-              ></img>
-            </a>
-          </div>
+          <a href="https://forms.gle/nGuxmLWthdBdrhn7A">
+            <button data-aos="zoom-in">Register Now</button>
+          </a>
+        </div>
+        <div className="logos">
+          <p>Powered By</p>
+          <a href="https://www.vvce.ac.in">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/vvceLogo.png`}
+              alt="vvce"
+            ></img>
+          </a>
+          <a href="https://www.vvce.ac.org">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/acmLogo.png`}
+              alt="acm"
+            ></img>
+          </a>
         </div>
       </div>
     );
