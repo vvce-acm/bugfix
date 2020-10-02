f230b84cea954a547eac25b5ab3bf025503b091a
Suraj-Gov <surajgovind332000@gmail.com>
2020-10-02 17:59:36 +0530

    build is now added to git

---
+++ build/precache-manifest.c5166c4e9192d19bb5c74787f7aa8538.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661be1b407c353fd520dc19c490c8c86",
+    "url": "/bugfix/index.html"
+  },
+  {
+    "revision": "fdbb91d9d4e2615299b5",
+    "url": "/bugfix/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "6fd072a8a1aaec9602b3",
+    "url": "/bugfix/static/css/main.97691d55.chunk.css"
+  },
+  {
+    "revision": "fdbb91d9d4e2615299b5",
+    "url": "/bugfix/static/js/2.ff89dcd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bugfix/static/js/2.ff89dcd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd072a8a1aaec9602b3",
+    "url": "/bugfix/static/js/main.6a0bf4ef.chunk.js"
+  },
+  {
+    "revision": "a9074b7d886c7085e982",
+    "url": "/bugfix/static/js/runtime-main.b0e6552a.js"
+  }
+]);
